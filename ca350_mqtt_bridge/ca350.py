b8a1c0c5b4e905e094cb0341ba45d02969fc6225
Peter <peter_p@aon.at>
2026-03-19 23:44:01 +0100

    fix: config.yaml-->added homeassistant_api

--- ca350_mqtt_bridge/ca350.py
+++ ca350_mqtt_bridge/ca350.py
@@ -68,7 +68,8 @@ class MqttManager:
     def __init__(self):
         self.ca = None
         self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "CA350")
-        self.client.username_pw_set(mqtt_user, mqtt_pass)
+        if mqtt_user:
+            self.client.username_pw_set(mqtt_user, mqtt_pass)
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         self.client.on_disconnect = self.on_disconnect
